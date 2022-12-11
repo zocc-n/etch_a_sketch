52f06803a450a9be824b75c3e2e3e5d63df756c0
zocc-n <znanic@yahoo.com>
2022-12-11 20:54:49 +0100

    Add function to change color and to set number of elements

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const main = document.querySelector('main');
 
-for(let i=1; i<5; i++){
-    for(let j=1; j<5; j++){
-        main.innerHTML += `<div class="field" class="row_${i}" id="${i + '_' +j}"></div>`;
+let numOfElements = 16;
+
+for(let i=1; i<=numOfElements; i++){
+    for(let j=1; j<=numOfElements; j++){
+        main.innerHTML += `<div onclick="changeColor(this)" class="field" class="row_${i}" id="${i + '_' +j}"></div>`;
     }
+}
+
+main.style.gridTemplateColumns = 'auto '.repeat(numOfElements);
+
+function changeColor(e){
+    document.getElementById(e.id).style.background = 'goldenrod';
 }

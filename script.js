1a48609bc0d0665a07a581fe91cc1f089be476c3
zocc-n <znanic@yahoo.com>
2022-12-12 02:49:58 +0100

    New functionalities, improved code

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const main = document.querySelector('main');
+let selectedColor = 'black';
 
-let numOfElements = 16;
-
-for(let i=1; i<=numOfElements; i++){
-    for(let j=1; j<=numOfElements; j++){
-        main.innerHTML += `<div onclick="changeColor(this)" class="field" class="row_${i}" id="${i + '_' +j}"></div>`;
+function drawSquares(numOfElements){
+    main.innerHTML = '';
+    for(let i=1; i<=numOfElements; i++){
+        for(let j=1; j<=numOfElements; j++){
+            main.innerHTML += `<div onmouseover="colorElement(this)" class="field" class="row_${i}" id="${i + '_' +j}"></div>`;
+        }
     }
+    main.style.gridTemplateColumns = 'auto '.repeat(numOfElements);
 }
 
-main.style.gridTemplateColumns = 'auto '.repeat(numOfElements);
-
-function changeColor(e){
-    document.getElementById(e.id).style.background = 'goldenrod';
+function colorElement(e){
+    document.getElementById(e.id).style.background = selectedColor;
 }
+
+document.getElementById('colorMenu').addEventListener('change', function() {
+     selectedColor = this.value;
+});
+
+function changeSquareNum(){
+    numOfElements = document.getElementById('elemNumForm').value;
+    drawSquares(numOfElements);
+}
+
+drawSquares(16);
